cd661e7923ab3c9eba62e8b501d54732431820ba
jonathan_b <jonathan.bailleul@g2metric.com>
2023-08-10 10:36:13 +0200

    refactor/bugfix: program options for TCP client. Better time resolution for small packets. Bugfix: add data agregation into timing

--- src/TCP_client.cpp
+++ src/TCP_client.cpp
@@ -1,14 +1,36 @@
 //
-// Created by j.bailleul on 09/08/2023.
+// Created by j.bailleul on 09/08/2023 from:
+// https://gwenael-dunand.developpez.com/tutoriels/cpp/boost/asio/
 //
 
+#include <boost/program_options.hpp>
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
 #include <boost/chrono.hpp>
 
-int main()
+
+/// structure remplie par ligne de commande
+struct progArgs
 {
+  std::string server_ipv4; // ipv4 adress string for server
+  size_t      server_port; // port number
+  size_t      tcp_packet_size; // taille des paquets TCP qui vont découper la payload à transmettre
+  char        value; // valeur unique de tous les élts de la payload
+};
+
+static void usage(int argc, char **argv, progArgs &a);
+
+const char *srv_ipv4_g; /// global contenant
+
+
+int main(int argc, char **argv)
+{
+	// usage & argument parsing
+	progArgs args;
+	usage(argc, argv, args);
+	
+	
 	boost::chrono::high_resolution_clock::time_point t1, t2, t3;
 	
 	
@@ -19,35 +41,47 @@ int main()
 	using ip::tcp;
 	
 	// On veut se connecter sur la machine locale, port 7171
-	tcp::endpoint endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 7171);
+	tcp::endpoint endpoint(boost::asio::ip::address::from_string(args . server_ipv4), args . server_port);
 	
 	// On crée une socket // (1)
 	tcp::socket socket(ios);
 	
 	// Tentative de connexion, bloquante // (2)
-	socket.connect(endpoint);
+	socket . connect(endpoint);
 	
-	// Création du buffer de réception // (3)
-	const size_t buffer_size = 1024*256;
-	boost::array<char, buffer_size> buf;
-	
+	// Création du buffer de réception (d'un paquet) // (3)
+	const size_t           buffer_size = args . tcp_packet_size;
+	boost::array<char, 64> buf;
+	void                   *buffer_data = malloc(buffer_size);
+	// message agrégé (paquets collés ensemble)
+	void* data_msg = _aligned_malloc(1024*1024*512* sizeof(char), 1024); // taille totale inconnue, maximum gérée = 512Mo;
+	if (data_msg == nullptr || buffer_data == nullptr) return 1;
 	
 	// on découpe le message en série de buffers de buffer_size
 	t1 = boost::chrono::high_resolution_clock::now();
 	size_t total_len = 0;
+	void* write_position = data_msg;
+	
 	while (1)
 		{
 			boost::system::error_code error;
-			// Réception des données, len = nombre d'octets reçus // (4)
-			size_t len = socket.read_some(boost::asio::buffer(buf), error);
+			// Réception des données. len : nombre d'octets reçus // (4)
+			size_t len = socket . read_some(boost::asio::buffer(buffer_data, buffer_size), error);
+			// read_some(boost::asio::buffer(buf), error);
+			
+			// agrégation au message (si paquets consécutifs)
+			memcpy(write_position, buffer_data, buffer_size);
+			char* wrt_pos = static_cast<char *>(write_position);
+			wrt_pos += buffer_size;
+			write_position = static_cast<void*>(wrt_pos);
 			total_len += len;
 			
-			if (error == boost::asio::error::eof) // (5)
+			if ( error == boost::asio::error::eof ) // (5)
 				{
 					std::cout << "\nTermine !" << std::endl;
 					break;
 				}
-			
+
 #if 0
 			// On affiche (6)
 			std::cout << "\npayload size:" << len << "\nmessage lu du serveur:";
@@ -55,14 +89,71 @@ int main()
 				std::cout.write(buf.data(), len);
 #endif
 		}
+		
+	// mesure à la fin de réception des données
 	t2 = boost::chrono::high_resolution_clock::now();
-	auto duration1 = (boost::chrono::duration_cast<boost::chrono::milliseconds>(t2-t1));
-	std::cout << "\nclient: durations (ms):" << duration1.count();
+	auto duration1 = (boost::chrono::duration_cast<boost::chrono::microseconds>(t2 - t1));
+	std::cout << "\nclient: durations (µs):" << duration1 . count();
 	
 	float payload_mb = static_cast<float>(total_len) / (1024 * 1024);
-	float bandwidth = payload_mb * (1000.0f / duration1.count());
+	float bandwidth  = payload_mb * (1000 * 1000.0f / duration1 . count());
 	
 	std::cout << "\nclient: bandwidth (Mo/s): " << bandwidth << " (packet size: " << buffer_size << ")";
 	
 	return 0;
 }
+
+
+// ==================================================================
+
+/*!
+ * @brief usage: affiche aide par défaut en ligne de commande si aucun argument.
+ * Et fait le lien entre les arguments saisis et la struct progArgs
+ * @param argc
+ * @param argv
+ * @param a
+ */
+static void
+usage(int argc, char **argv, progArgs &a)
+{
+	// initial values
+	
+	
+	// let the parsing commence
+	namespace po = boost::program_options;
+	po::options_description desc("This program lance un serveur IPC qui va transférer, à tout client, une payload de taille définie en argt");
+	
+	desc . add_options()
+		     ("help,h", "Print help messages")    // no space after comma
+		     ("srvip,i", po::value<std::string>(&a . server_ipv4) -> default_value("127.0.0.1"), \
+             "ipv4 adress for tcp server")
+		     ("port", po::value<size_t>(&a . server_port) -> required(), \
+             "port to read from tcp server")
+		     ("packetsize,p", po::value<size_t>(&a . tcp_packet_size) -> required(), \
+             "size (in bytes/octets) of tcp packets to cut the payload into")
+		     ("value,v", po::value<char>(&a . value) -> default_value(3), \
+                 "Value of all bytes in payload");
+	
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc),
+	          vm);
+	
+	if ( vm . count("help"))
+		{
+			std::cout << desc << "This is the basic help message\n";
+			exit(EXIT_SUCCESS);
+		}
+	
+	
+	try
+		{
+			po::notify(vm); // will throw if a required option is not set: usually, call help first
+		}
+	catch (po::error &e)
+		{
+			std::cerr << desc;
+			exit(EXIT_FAILURE);  // show usage even if args broke
+		}
+	
+}
+
--- src/TCP_server_sync.cpp
+++ src/TCP_server_sync.cpp
@@ -1,39 +1,63 @@
 //
-// Created by j.bailleul on 09/08/2023.
+// Created by j.bailleul on 09/08/2023 from:
 // https://gwenael-dunand.developpez.com/tutoriels/cpp/boost/asio/
 //
+
 #define _WIN32_WINNT 0x0501 // Windows XP
 
+#include <boost/program_options.hpp>
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
 #include <iostream>
 #include <cstdlib>
 
+
+/// structure remplie par ligne de commande
+struct progArgs
+{
+  std::string server_ipv4; // ipv4 adress string for server
+  size_t      server_port; // port number
+  size_t      sm_size_bytes; // payload size in octets / Bytes
+  size_t      sm_size_mega;  // same in Mo / Mb : ignoré par la suite
+  char        value; // valeur unique de tous les élts de la payload
+};
+
+static void usage(int argc, char **argv, progArgs &a);
+
+const char *srv_ipv4_g; /// global contenant
+
+
 using boost::asio::ip::tcp;
 
-int main()
+int main(int argc, char**argv)
 {
-	size_t payload_size = 1024*1024*256 *sizeof(char);
-	void* data = malloc(payload_size); // _aligned_malloc(payload_size, 1024) : pareil;
-	if (data == nullptr) return 1;
+	// usage & argument parsing
+	progArgs args;
+	usage(argc, argv, args);
 	
-	char* data_char = static_cast<char*>(data);
-	for (size_t i = 0; i < payload_size; i++)
-		data_char[i] = 'h';
-	const char* toto = reinterpret_cast<const char*>( data );
 	
-	std::vector<char> vecdata(1024*1024*256*sizeof(char));
-	for (auto &e : vecdata)
-		e = 'g';
+	
+	size_t payload_size = args.sm_size_bytes; //1024 * 1024 * 256 * sizeof(char);
+	void *data = malloc(payload_size); // _aligned_malloc(payload_size, 1024) : pareil;
+	if ( data == nullptr ) return 1;
+	
+	char *data_char = static_cast<char *>(data);
+	for (size_t i = 0; i < payload_size; i ++)
+		data_char[i] = args.value;
+	const char *payload_buffer = reinterpret_cast<const char *>( data );
+	
+	std::vector<char> vecdata(args.sm_size_bytes);
+	for (auto         &e: vecdata)
+		e = args.value;
 	
 	
 	// Création du service principal et du résolveur.
 	boost::asio::io_service ios;
 	
 	// Création de l'acceptor avec le port d'écoute 7171 et une adresse quelconque de type IPv4 // (1)
-	tcp::acceptor acceptor(ios, tcp::endpoint(tcp::v4(), 7171));
+	tcp::acceptor acceptor(ios, tcp::endpoint(tcp::v4(), args.server_port));
 	
-	std::string msg ("Bienvenue sur le serveur !"); // (2)
+	std::string msg("Bienvenue sur le serveur !"); // (2)
 	// On attend la venue d'un client
 	while (1)
 		{
@@ -41,14 +65,73 @@ int main()
 			tcp::socket socket(ios); // (3)
 			
 			// On accepte la connexion
-			acceptor.accept(socket); // (4)
+			acceptor . accept(socket); // (4)
 			std::cout << "Client recu ! " << std::endl;
 			
-			// On envoi un message de bienvenue
+			// On envoie un message de bienvenue
 			// socket.send(boost::asio::buffer(msg)); // (5)
 			// socket.send( boost::asio::buffer(vecdata));
-			socket. send( boost::asio::buffer(toto, payload_size));
+			socket . send(boost::asio::buffer(payload_buffer, payload_size));
 		}
-		
+	
 	return 0;
 }
+
+// ==================================================================
+
+/*!
+ * @brief usage: affiche aide par défaut en ligne de commande si aucun argument.
+ * Et fait le lien entre les arguments saisis et la struct progArgs
+ * @param argc
+ * @param argv
+ * @param a
+ */
+static void
+usage(int argc, char **argv, progArgs &a)
+{
+	// initial values
+	a . sm_size_bytes = 1024 * 1024;
+	
+	// let the parsing commence
+	namespace po = boost::program_options;
+	po::options_description desc("This program lance un serveur IPC qui va transférer, à tout client, une payload de taille définie en argt");
+	
+	desc . add_options()
+		     ("help,h", "Print help messages")    // no space after comma
+		     ("srvip,i", po::value<std::string>(&a . server_ipv4) -> default_value("127.0.0.1"), \
+             "ipv4 adress for tcp server")
+		     ("port,p", po::value<size_t>(&a.server_port)->required(), \
+			 "port to read from tcp server")
+		     ("sizeMB", po::value<size_t>(&a . sm_size_mega), \
+                 "Set payload size in megaBytes/méga-octets")
+		     ("sizeB", po::value<size_t>(&a . sm_size_bytes), \
+             "Set payload size in bytes/octets")
+		     ("value,v", po::value<char>(&a . value) -> default_value(3), \
+                 "Value of all bytes in payload");
+	
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc),
+	          vm);
+	
+	if ( vm . count("help"))
+		{
+			std::cout << desc << "This is the basic help message\n";
+			exit(EXIT_SUCCESS);
+		}
+	
+	
+	try
+		{
+			po::notify(vm); // will throw if a required option is not set: usually, call help first
+		}
+	catch (po::error &e)
+		{
+			std::cerr << desc;
+			exit(EXIT_FAILURE);  // show usage even if args broke
+		}
+	
+	
+	if ( vm . count("sizeMB"))
+		a . sm_size_bytes = a . sm_size_mega * 1024 * 1024;
+}
+
